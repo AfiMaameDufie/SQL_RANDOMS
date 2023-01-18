d9514ce578a73f420d6d0286a83e52a980a24386
abigail.gbadago <afimaamedufie@gmail.com>
2023-01-18 17:34:45 +0000

    Queries for Selecting, Distinct and Count

---
+++ datacamp_queries.sql
@@ -0,0 +1,12 @@
+SELECT release_year FROM films
+SELECT title FROM films
+SELECT name FROM people
+
+SELECT title
+FROM films;
+
+SELECT title, release_year
+FROM films;
+
+SELECT title, release_year, country
+FROM films;
