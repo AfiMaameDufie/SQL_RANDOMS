9cc67211f90054ff67c7ab192ed8ba21a0693f20
abigail.gbadago <afimaamedufie@gmail.com>
2023-01-18 17:43:47 +0000

    Queries for Distinct and Count

--- datacamp_queries.sql
+++ datacamp_queries.sql
@@ -10,3 +10,31 @@ FROM films;
 
 SELECT title, release_year, country
 FROM films;
+
+SELECT DISTINCT countryx
+FROM films;
+
+SELECT DISTINCT certification
+FROM films;
+
+SELECT DISTINCT role
+FROM roles
+
+SELECT COUNT(*)
+FROM reviews;
+
+SELECT COUNT(*)
+FROM people;
+
+
+SELECT COUNT(birthdate)
+FROM people;
+
+SELECT COUNT(DISTINCT birthdate)
+FROM people;
+
+SELECT COUNT (DISTINCT language)
+FROM films;
+
+SELECT COUNT (DISTINCT country)
+FROM films;
